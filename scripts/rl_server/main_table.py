0c36986a63289f135806dfc5396a448986db4847
nader <nader.zare88@gmail.com>
2022-12-09 21:25:06 -0400

    add python rl code

---
+++ scripts/rl_server/main_table.py
@@ -0,0 +1,185 @@
+import time
+
+from redis_server import RedisServer
+
+
+class StepData:
+    def __init__(self):
+        self.state = None
+        self.next_state = None
+        self.reward = None
+        self.action = None
+        self.done = False
+
+from table import QTable
+rl = QTable()
+
+rd = RedisServer()
+rd.client.flushdb()
+
+data: dict[int, StepData] = {}
+
+
+def add_trainer_info(pre_num_cycle, values):
+    cycle = int(pre_num_cycle.split('_')[-1])
+    is_done = int(values[0]) >= 2
+    is_start = int(values[0]) == 0
+    status = int(values[0])
+    if is_start:
+        return is_start, is_done, status, 0
+    reward_cycle = cycle - 1
+    if reward_cycle not in data.keys():
+        data[reward_cycle] = StepData()
+    data[reward_cycle].done = is_done
+    data[reward_cycle].reward = values[1]
+    return is_start, is_done, status, data[reward_cycle].reward
+
+
+def add_player_info(pre_num_cycle, values):
+    cycle = int(pre_num_cycle.split('_')[-1])
+    if cycle not in data.keys():
+        data[cycle] = StepData()
+    data[cycle].state = values
+    cycle -= 1
+    if cycle not in data.keys():
+        data[cycle] = StepData()
+    data[cycle].next_state = values
+
+
+def add_player_action(pre_num_cycle, action):
+    cycle = int(pre_num_cycle.split('_')[-1])
+    if cycle not in data.keys():
+        data[cycle] = StepData()
+    data[cycle].action = action
+
+
+def add_data_to_buffer():
+    global data
+    should_remove = []
+    for key in data.keys():
+        if data[key].next_state is None and data[key].done is False:
+            continue
+        if data[key].reward is None:
+            continue
+        if data[key].state is None:
+            continue
+        if data[key].action is None:
+            continue
+        rl.add_to_buffer(data[key].state, data[key].action, data[key].reward, data[key].next_state)
+        should_remove.append(key)
+    for key in should_remove:
+        del data[key]
+
+
+patch_number = 0
+patch_number_max = 100
+train_episode_number = 0
+train_episode_number_max = 100
+test_episode_number = 0
+test_episode_number_max = 10
+is_train = True
+received_messages = {}
+player_ignore_cycles = []
+training_cycles = []
+testing_cycles = []
+training_step_rewards = []
+testing_step_rewards = []
+training_episode_last_rewards = []
+testing_episode_last_rewards = []
+training_episode_com_rewards = []
+testing_episode_com_rewards = []
+training_episode_res = []
+testing_episode_res = []
+latest_episode_rewards = []
+
+def end_function():
+    print('EEEEEEEEEEEENNNNNNNNNDDDDDDDDDDD')
+    f = open('player_ignore_cycles', 'w')
+    f.write('\n'.join([str(i) for i in player_ignore_cycles]))
+    f = open('training_cycles', 'w')
+    f.write('\n'.join([str(i) for i in training_cycles]))
+    f = open('testing_cycles', 'w')
+    f.write('\n'.join([str(i) for i in testing_cycles]))
+    f = open('training_step_rewards', 'w')
+    f.write('\n'.join([str(i) for i in training_step_rewards]))
+    f = open('testing_step_rewards', 'w')
+    f.write('\n'.join([str(i) for i in testing_step_rewards]))
+    f = open('training_episode_last_rewards', 'w')
+    f.write('\n'.join([str(i) for i in training_episode_last_rewards]))
+    f = open('testing_episode_last_rewards', 'w')
+    f.write('\n'.join([str(i) for i in testing_episode_last_rewards]))
+    f = open('training_episode_com_rewards', 'w')
+    f.write('\n'.join([str(i) for i in training_episode_com_rewards]))
+    f = open('2', 'w')
+    f.write('\n'.join([str(i) for i in testing_episode_com_rewards]))
+    f = open('training_episode_res', 'w')
+    f.write('\n'.join([str(i) for i in training_episode_res]))
+    f = open('testing_episode_res', 'w')
+    f.write('\n'.join([str(i) for i in testing_episode_res]))
+    f = open('latest_episode_rewards', 'w')
+    f.write('\n'.join([str(i) for i in latest_episode_rewards]))
+    while True:
+        time.sleep(1)
+i = 0
+cycle = None
+while True:
+    pre_num_cycle, values = rd.get_from_wait(0, cycle)
+    if pre_num_cycle is not None:
+        # print(pre_num_cycle, values)
+        cycle = int(pre_num_cycle.split('_')[-1])
+        is_start, is_done, status, reward = add_trainer_info(pre_num_cycle, values)
+        if not is_start:
+            # if is_train:
+            #     training_step_rewards.append(reward)
+            # else:
+            #     testing_step_rewards.append(reward)
+            latest_episode_rewards.append(reward)
+        received_messages[pre_num_cycle] = values
+        rd.set(pre_num_cycle, [1])
+        if is_start:  # start
+            pass
+        elif is_done:  # end
+            if is_train:
+                training_episode_last_rewards.append(reward)
+                training_episode_res.append(status)
+                training_episode_com_rewards.append(sum(latest_episode_rewards))
+                latest_episode_rewards = []
+                train_episode_number += 1
+                if train_episode_number == train_episode_number_max:
+                    is_train = False
+                    train_episode_number = 0
+            else:
+                testing_episode_last_rewards.append(reward)
+                testing_episode_res.append(status)
+                testing_episode_com_rewards.append(sum(latest_episode_rewards))
+                print(testing_episode_com_rewards[-1])
+                latest_episode_rewards = []
+                test_episode_number += 1
+                if test_episode_number == test_episode_number_max:
+                    is_train = True
+                    test_episode_number = 0
+                    patch_number += 1
+            if patch_number == patch_number_max:
+                end_function()
+    # if is_train:
+    #     training_cycles.append(cycle)
+    # else:
+    #     testing_cycles.append(cycle)
+    pre_num_cycle, values = rd.get_from_wait(1, cycle, wait_time_second=0.5)
+    if pre_num_cycle is not None:
+        if len(values) == 1:  # Fake
+            rd.set(pre_num_cycle, [0])
+        else:
+            # print(pre_num_cycle, values)
+            add_player_info(pre_num_cycle, values)
+            action = [rl.get_random_action_c(values, patch_number, 0.2 if is_train else 0.0)]
+            # print('###################################')
+            # print(type(action), action)
+            rd.set(pre_num_cycle, action)
+            add_player_action(pre_num_cycle, action[0])
+            received_messages[pre_num_cycle] = values
+    else:
+        player_ignore_cycles.append(cycle)
+    i += 1
+    cycle += 1
+    add_data_to_buffer()
---
+++ scripts/rl_server/table.py
@@ -0,0 +1,69 @@
+import random
+from typing import List
+from reply_buffer_simple import Buffer
+from transit import Transition
+
+
+class QTable:
+    def __init__(self, buffer_size=100000, train_interval_step=1):
+        self.q_table = []
+        for s1 in range(0, 36):
+            self.q_table.append([])
+            for a1 in range(0, 36):
+                self.q_table[-1].append(0)
+        self.buffer = Buffer(buffer_size)
+        self.train_interval_step = train_interval_step
+        self.transitions: List[Transition] = []
+        self.gama = 0.95
+        self.episode_number = 0
+        self.plan_number = 0
+        self.step_number = 0
+        self.loss_values = []
+        self.target_model_update = 2.005
+
+    def discretize_state(self, state):
+        s1 = int(state[1] * 360.0 / 10)
+        return s1
+
+    def discretize_action(self, action):
+        a = int(action * 360.0 / 10.0)
+        return a
+
+    def continues_action(self, action):
+        return action * 10.0 / 360.0
+
+    def get_best_action_d(self, state_c):
+        state_d = self.discretize_state(state_c)
+        max_q = self.q_table[state_d][0]
+        a_i = 0
+        for i in range(36):
+            if self.q_table[state_d][i] > max_q:
+                max_q = self.q_table[state_d][i]
+                a_i = i
+        return a_i
+
+    def get_random_action_c(self, state_c, patch_number, p_rnd=None):
+        best_action_d = self.get_best_action_d(state_c)
+        if random.random() < p_rnd:
+            best_action_d = random.randint(0, 35)
+        return self.continues_action(best_action_d)
+
+    def add_to_buffer(self, state, action, reward, next_state=None):
+        self.buffer.add(Transition(state, action, reward, next_state))
+        self.step_number += 1
+        self.update_from_buffer()
+
+    def update_from_buffer(self):
+        transits: List[Transition] = self.buffer.get_rand(32)
+        if len(transits) == 0:
+            return
+        for t in transits:
+            d_state = self.discretize_state(t.state)
+            d_action = self.discretize_action(t.action)
+            max_q_s = 0
+            if t.next_state is not None:
+                next_state_best_action_d = self.get_best_action_d(t.next_state)
+                d_next_state_d = self.discretize_state(t.next_state)
+                max_q_s = self.q_table[d_next_state_d][next_state_best_action_d]
+            self.q_table[d_state][d_action] += 0.1 * (t.reward + 0.95 * (max_q_s - self.q_table[d_state][d_action]))
+
