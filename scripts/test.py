aecdeefcb260481b3ef31b155c0eb1eb16d7a424
Aref <arefsayareh@gmail.com>
2023-02-05 16:18:51 -0330

    testing

--- scripts/test.py
+++ scripts/test.py
@@ -2,19 +2,35 @@ from tensorflow.keras.models import load_model
 
 from data import get_test_data
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 x, y, episode_duration = get_test_data()
 print(x[0][0])
+print(x[1][0])
+print(x[episode_duration-1][0])
 print(y[0])
 
 y = np.array(y)
-
-model = load_model('model')
-
-for i in range(episode_duration):
-    yp = model.predict_on_batch(x[i])
-    print(y - yp)
-
-
-model.save('model')
+#
+# model = load_model('model')
+#
+# acc = []
+# for i in range(episode_duration):
+#     episodes = np.array(x[i])
+#     print(episodes.shape)
+#     yp = model.predict(episodes)
+#     acc.append(np.mean(np.abs(y - yp), axis=0))
+#
+# x_err = []
+# y_err = []
+#
+# for a in acc:
+#     x_err.append(a[0])
+#     y_err.append(a[1])
+# plt.plot(x_err)
+# plt.savefig('x.png')
+# plt.close()
+#
+# plt.plot(y_err)
+# plt.savefig('y.png')
+# plt.close()
