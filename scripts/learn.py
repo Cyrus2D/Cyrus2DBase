0bd7c777030a2319070a55bb253b13865bc6207b
Aref <arefsayareh@gmail.com>
2023-02-06 15:31:54 -0330

    training

--- scripts/learn.py
+++ scripts/learn.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
-from data import get_data
+from data import get_data, create_headers
 import numpy as np
+
+from noise_accuracy import Config
 
 TRAIN_PERCENT = 0.7
 NX = 46
@@ -39,9 +41,42 @@ def create_model_DNN(episode_duration):
     return model
 
 
-x, y, episode_duration = get_data(100)
-x = np.array(x)
-y = np.array(y)
+def create_x_y_indexes(headers: dict[str, list[int]]):
+    x_indexes = []
+    for key, value in headers.items():
+        if key in ['cycle']:
+            continue
+        if key.find('full') != -1:
+            continue
+        x_indexes += value
+
+    y_indexes = headers['opp-5-full'][:-1]
+    return x_indexes, y_indexes
+
+def normalize_data(x, y):
+    config = Config()
+    pos_x_i = [i for i in range(0, 69, 3)]
+    pos_y_i = [i for i in range(1, 69, 3)]
+    pos_count_i = [i for i in range(2, 69, 3)]
+
+    x[:, pos_x_i] /= config.max_x
+    x[:, pos_y_i] /= config.max_y
+    x[:, pos_count_i] /= 30
+
+    y[:, 0] /= config.max_x
+    y[:, 1] /= config.max_y
+
+headers = create_headers()
+x_indexes, y_indexes = create_x_y_indexes(headers)
+xy = np.array(get_data(10))
+
+x = xy[:, x_indexes]
+y = xy[:, y_indexes]
+print(x)
+print(y)
+normalize_data(x, y)
+print(x)
+print(y)
 
 print(x.shape)
 print(y.shape)
