e39e4a7c68f3bf2080fa8c9cdbd70ece914826ae
Aref <arefsayareh@gmail.com>
2023-02-08 18:35:48 -0330

    labeling y

--- scripts/learn.py
+++ scripts/learn.py
@@ -31,20 +31,21 @@ def create_model_LSTM(episode_duration):
 
 def create_model_DNN(episode_duration):
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Dense(32, activation='relu', input_shape=(NX * episode_duration,)))
+    model.add(tf.keras.layers.Dense(32, activation='relu', input_shape=(NX,)))
     model.add(tf.keras.layers.Dense(16, activation='relu'))
     model.add(tf.keras.layers.Dense(2, activation='linear'))
 
     model.compile(optimizer='adam', loss='mse')
     return model
 
+
 def create_model_DNN_softmax(episode_duration):
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Dense(512, activation='relu', input_shape=(NX * episode_duration,)))
-    model.add(tf.keras.layers.Dense(256, activation='relu'))
-    model.add(tf.keras.layers.Dense(NY**2, activation='softmax'))
+    model.add(tf.keras.layers.Dense(64, activation='relu', input_shape=(NX,)))
+    model.add(tf.keras.layers.Dense(32, activation='relu'))
+    model.add(tf.keras.layers.Dense(NY ** 2, activation='softmax'))
 
-    model.compile(optimizer='adam', loss='mse')
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
 
@@ -57,7 +58,8 @@ y = create_labeled_y(xy, NY, 20)
 print(xy.shape)
 
 print('seperating')
-x = xy[:, :, x_indexes]
+# x = xy[:, :, x_indexes]
+x = xy[:, x_indexes]
 
 print(x.shape)
 print(y.shape)
@@ -77,6 +79,6 @@ y = y[r_indexes]
 print(x.shape)
 print(y.shape)
 
-model = create_model_RNN(episode_duration)
+model = create_model_DNN_softmax(episode_duration)
 model.fit(x, y, batch_size=64, epochs=3, validation_split=0.1)
 model.save('softmax-model')
